8111c9fad3d5618ebe360bc68f70ae47f234b12e
Tralex-W <eric.wramling@gmail.com>
2026-01-15 14:40:50 +0100

    able to plot mulitple caffeine intakes in one graph (not yet added up)

--- visualize.py
+++ visualize.py
@@ -10,14 +10,19 @@ with open("beverage_data.json", "r") as f:
 
 
 # caffeine_intake_mg = caffeine in mg at time 0
-decay_constant = 0.1385  # decay_constant = 0.1385 = ln2/5, because of T1/2=ln2/k=5
+CAFFEINE_HALF_LIFE_H = 5
+DECAY_CONSTANT = np.log(2) / CAFFEINE_HALF_LIFE_H  # because of T1/2=ln2/k=5
+WINDOW_WIDTH = 12
+WINDOW_HEIGHT = 6
 
 
-def visualize_caffeine_concentration_over_time(caffeine_intake_ml, intake_time, user):
-    global decay_constant
+def plot_caffeine_concentration_over_time_for_one_intake(
+    caffeine_intake_ml, intake_time, user
+):
+    global DECAY_CONSTANT
 
     x = np.linspace(0, 24, 100)
-    y = caffeine_intake_ml * np.exp(-decay_constant * (x - intake_time))
+    y = caffeine_intake_ml * np.exp(-DECAY_CONSTANT * (x - intake_time))
 
     x_plot = x[x >= intake_time]
     y_plot = y[x >= intake_time]
@@ -27,29 +32,37 @@ def visualize_caffeine_concentration_over_time(caffeine_intake_ml, intake_time, 
     plt.xlabel("Zeit t in Stunden nach 0 Uhr")
     plt.ylabel("Koffeingehalt in mg")
     plt.title(f"Koffeingehalt des Konsumenten {user} über den Tag")
+
+
+def visualize_caffeine_concentration_for_all_caffein_intakes_of_a_user(user_idx):
+    global WINDOW_HEIGHT, WINDOW_WIDTH
+
+    user = user_caffeine_intake["users"][user_idx]["name"]
+    plt.figure(figsize=(WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    for caffeine_intake_idx in range(0, 2):  # each caffeine intake
+        intake_time = float(
+            user_caffeine_intake["users"][user_idx]["caffeine_intake"][
+                caffeine_intake_idx
+            ]["time"]
+        )
+        amount_of_beverage_ml = user_caffeine_intake["users"][user_idx][
+            "caffeine_intake"
+        ][caffeine_intake_idx]["amount_ml"]
+        beverage = user_caffeine_intake["users"][user_idx]["caffeine_intake"][
+            caffeine_intake_idx
+        ]["source"]
+        beverage_caffeine_concentration_mg_per_ml = beverages[beverage]
+        amount_of_caffeine_mg = (
+            beverage_caffeine_concentration_mg_per_ml * amount_of_beverage_ml
+        )
+
+        plot_caffeine_concentration_over_time_for_one_intake(
+            amount_of_caffeine_mg, intake_time, user
+        )
+
     plt.show()
 
 
 user_idx = 0
-caffeine_intake_idx = 0
-
-
-user = user_caffeine_intake["users"][user_idx]["name"]
-intake_time = float(
-    user_caffeine_intake["users"][user_idx]["caffeine_intake"][caffeine_intake_idx][
-        "time"
-    ]
-)
-amount_of_beverage_ml = user_caffeine_intake["users"][user_idx]["caffeine_intake"][
-    caffeine_intake_idx
-]["amount_ml"]
-beverage = user_caffeine_intake["users"][user_idx]["caffeine_intake"][
-    caffeine_intake_idx
-]["source"]
-beverage_caffeine_concentration_mg_per_ml = beverages[beverage]
-amount_of_caffeine_mg = (
-    beverage_caffeine_concentration_mg_per_ml * amount_of_beverage_ml
-)
-
-
-visualize_caffeine_concentration_over_time(amount_of_caffeine_mg, intake_time, user)
+visualize_caffeine_concentration_for_all_caffein_intakes_of_a_user(user_idx)
